3f9c49f3d41fef431493cc76540e103e59e02853
msopansopian <m.sopian369@gmail.com>
2025-03-09 13:02:08 +0700

    fix : perbaikan tampilan dan ubah index.html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,10 +7,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: 'index.html',
-        login: 'Login.html',
-        register: 'Register.html'
-      }
-    }
-  }
-})
+        index: "index.html",
+        register: "Register.html",
+      },
+    },
+  },
+});
